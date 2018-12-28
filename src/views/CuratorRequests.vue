3414a9121cfa31ea7418ff4610e718b24f9ebed2
ermakov4as <ermakov4as@gmail.com>
2018-12-29 00:39:14 +0300

    v0.0.2.11

--- src/views/CuratorRequests.vue
+++ src/views/CuratorRequests.vue
@@ -2,35 +2,130 @@
     <div>
         <h1 class="component-title">Заявки на разбор или озвучку</h1>
         
-        <!-- Отображение спиннера, если нет соединения с сервером -->
-        <!-- При div почему-то не работает... -->
-        <p class="text-center" v-if="!dataReady">
-            <i class='fa fa-spinner fa-pulse fa-4x'></i>
-        </p>
-        
-        <!-- Гиперблок потоков, если есть соединение с сервером -->
-        <div class="component-content" v-else>
+        <div class="component-content">
             
             <!-- Блок выбора режима работы -->
-                <div class="form-group">
-                    <div class="label-subtitle">
-                        <label for="name">Выбор типа заявки</label>
-                    </div>
-                    <div class="d-flex justify-content-start">
-                        <div class="request-type-panel mr-4">Заявки на разбор</div>
-                        <div class="request-type-panel">Заявки на озвучку</div>
+            <div>
+                <!--<div class="label-subtitle">
+                    <label for="name">Выбор типа заявки</label>
+                </div>-->
+                <div class="d-flex justify-content-start control-block">
+                    <div class="request-type-panel mr-4" :class="{active: (mode === 'analysis')}" @click="analysis">Заявки на разбор</div>
+                    <div class="request-type-panel" :class="{active: (mode === 'voice')}" @click="voice">Заявки на озвучку</div>
 
-                        <div class="ml-auto request-type-checkbox">
-                            <label class="mr-3">Показать: </label>
-                            <input type="checkbox" id="non-completed" v-model="showNonCompleted">
-                            <label for="non-completed" class="mr-3">незавершённые</label>
-                            <input type="checkbox" id="completed" v-model="showCompleted">
-                            <label for="completed">завершённые</label>
-                        </div>
-
+                    <div class="ml-auto request-type-checkbox">
+                        <label class="mr-3">Показать: </label>
+                        <input type="checkbox" id="non-completed" class="checkbox mr-1" v-model="showNonCompleted">
+                        <label for="non-completed" class="mr-3">незавершённые</label>
+                        <input type="checkbox" id="completed" class="checkbox mr-1" v-model="showCompleted">
+                        <label for="completed">завершённые</label>
                     </div>
+
+                </div>
+            </div>
+
+            <!-- Отображение спиннера, если нет соединения с сервером -->
+            <!-- При div почему-то не работает... -->
+            <p class="text-center" v-if="!dataReady">
+                <i class='fa fa-spinner fa-pulse fa-4x'></i>
+            </p>
+
+            <!-- Гиперблок потоков, если есть соединение с сервером -->
+            <div class="table-place" v-else-if="mode != 'none'">
+                
+                <!-- The Table itself -->
+                <table v-if="mode === 'voice'" class="table table-bordered table-hover card-sentences-table">
+                    
+                   <!-- Заголовок таблицы -->
+                    <thead>
+                        <tr class="center">
+                            <th class="sizes-min no-vertical-padding">Дата<br>cоздания</th>
+                            <th class="sizes-min">Пользователь</th>
+                            <th class="lang-col">EN (Question)</th>
+                            <th class="lang-col">RU (Answer)</th>
+                            <th width="200">Комментарий</th>
+                            <th class="sizes-min">Исполнитель</th>
+                        </tr>
+                    </thead>
+
+                    <!-- Тело таблицы -->
+                    <tbody>
+
+                        <!-- Непроверенные попытки -->
+                        <tr v-for="(request, index) in requests" :key="index">
+                            <template v-if="(showNonCompleted && request.finished === null) || (showCompleted && request.finished != null)">
+                                        
+                                <!-- Автор и группа попытки -->
+                                <td class="center sizes-min" scope="row">{{ decodeDate(request.created) }}</td>
+                                <td class="center sizes-min" scope="row">{{ request.dict_unit.profile }}</td>
+                                        
+                                <!-- Английский текст попытки -->
+                                <td class="lang-col">
+                                    <div class="d-flex">
+                                        {{ request.dict_unit.question }}
+
+                                        <!-- Английская озвучка попытки, если есть -->
+                                        <div 
+                                                v-if="request.dict_unit.question_audio" 
+                                                class="material-icons pointer ml-auto" 
+                                                @click="playAudio(request.dict_unit.question_audio)">volume_up</div>
+
+                                    </div>
+                                </td>
+
+                                <!-- Русский текст попытки -->
+                                <td class="lang-col">
+                                    <div class="d-flex">
+                                        {{ request.dict_unit.answer }}
+
+                                        <!-- Русская озвучка попытки, если есть -->
+                                        <div 
+                                                v-if="request.dict_unit.answer_audio" 
+                                                class="material-icons pointer ml-auto" 
+                                                @click="playAudio(request.dict_unit.answer_audio)">volume_up</div>
+
+                                    </div>
+                                </td>
+
+                                <!-- Комментарий к попытке -->
+                                <td>
+                                    <!--<textarea
+                                            type="text"
+                                            id='comment'
+                                            class="form-control font-textarea"
+                                            :rows="countRows(request.dict_unit.comment)"
+                                            placeholder="Комментарий"
+                                            v-autosize="request.dict_unit.comment"
+                                            v-model="request.dict_unit.comment"></textarea>-->
+                                    {{ request.dict_unit.comment }}
+                                </td>
+
+                                <!-- Кнопка сохранения попытки -->
+                                <!--<td class="sizes-min align-middle">
+                                    <div class="btn btn-success" @click="saveAttempt(request)">Сохранить</div>
+                                </td>-->
+
+                                <td>
+                                    {{ request.dict_unit.comment.performer }}
+                                </td>
+
+                            </template>
+                        </tr>
+
+                    </tbody>
+
+                </table>        
+
+
+                <!--<div v-if="mode === 'analysis'">
+                    <p>!!!</p>
                 </div>
 
+                <div v-else-if="mode === 'voice'">
+
+                </div>-->
+
+            </div>
 
         </div>
 
@@ -45,16 +140,51 @@
             return {
                 dataReady: true,
                 showNonCompleted: true,
-                showCompleted: false
+                showCompleted: false,
+                mode: 'none',
+                requests: []
             }
         },
 
         methods: {
+            // Воспроизведение аудио
+            playAudio(source) {
+                if (this.snd) {
+                    this.snd.pause();
+                }
+                this.snd = new Audio(source);
+                this.snd.play();
+            },
+
+            decodeDate(date) {
+                let msUTS = Date.parse(date);
+                let normalDate = new Date(msUTS);
+                let creationDate = `${normalDate.getDate()}.${normalDate.getMonth()}.${normalDate.getFullYear()}`;
+                //console.log(creationDate);
+                return creationDate;
+            },
+
+            analysis() {
+                //this.dataLoaded = false;
+                //this.$set(this.flags, 'dataReady', false);
+                this.dataReady = false;
+                this.mode = 'analysis';
+                this.getData('/staff/curator/analysis_requests/');
+                //this.dataReady = true;
+            },
+
+            voice() {
+                this.dataReady = false;
+                this.mode = 'voice';
+                this.getData('/staff/curator/voice_requests/');
+            },
+
             // Загрузка данных
-            getData() {
-                HTTP.get('/staff/curator/attempt_list/')
+            getData(adress) {
+                HTTP.get(adress)
                     .then(response => {
-                        this.streams = response.data;
+                        console.log(response.data);
+                        this.requests = response.data;
                         this.dataReady = true;                          
                     })
                     .catch(error => {
@@ -70,17 +200,59 @@
         },
 
         watch: {
-
+            /*dataLoaded: {
+                handler(val, oldVal) {
+                    this.dataReady = this.dataLoaded;
+                }
+            }*/
         },
 
     }
 </script>
 
 <style scoped>
+    .no-vertical-padding {
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
+    .card-sentences-table {
+        margin-top: 10px;
+        margin-bottom: 5px;
+        vertical-align: middle;
+    }
+
+    .table-place {
+        margin-top: 0;
+        border: 1px solid grey;
+        border-top: none;
+        padding: 5px;
+    }
+
+    .active {
+        background-color: white;
+        margin-bottom: -1px;
+        margin-left: -1px;
+        margin-right: -1px;
+        border-right: 1px solid grey;
+        border-left: 1px solid grey;
+    }
+
+    .control-block {
+        border: 1px solid grey;
+        border-radius: 1px;
+        background-color: whitesmoke;
+    }
+
+    .checkbox {
+        transform: scale(1.3);
+        opacity: 0.9;
+        cursor: pointer;
+    }
+
     .request-type-panel {
         padding: 7px 65px 7px 65px;
-        background-color: blue;
-        border-radius: 3px;
+        border-radius: 1px;
         font-size: 21px;
     }
 
@@ -91,7 +263,6 @@
     }
 
     .request-type-checkbox {
-        background-color: green;
         border-radius: 3px;
         font-size: 18px;
         padding-top: 7px;
